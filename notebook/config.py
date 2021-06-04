7138d0ea9d254999dc3dee529bc0b4bc9a8ed34f
xin luo <xinluo_xin@163.com>
2021-06-04 09:50:15 +0800

    new commit

--- notebook/config.py
+++ notebook/config.py
@@ -1,3 +1,4 @@
 # root = '/content/drive/My Drive/landsat578_water'
-root = 'G:\\jupyter_work\\landsat578_water'
+root = '/Users/luo/OneDrive/Open-source-project/landsat578-water'  # luo
+# root = 'G:\\jupyter_work\\landsat578_water'    ## zhang
 
